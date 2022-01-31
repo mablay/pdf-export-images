62bac098a73746eeb32a22cb6f673197adc223e4
Marc Ablay <marc.ablay@gmail.com>
2022-01-31 23:47:57 +0100

    catched exception for inavlid nodes

--- src/export-images.js
+++ src/export-images.js
@@ -27,22 +27,26 @@ export async function exportImages (src, dst, emit = () => {}) {
     const ops = await page.getOperatorList()
 
     for (let i = 0; i < ops.fnArray.length; i++) {
-      if (
-        ops.fnArray[i] === OPS.paintJpegXObject ||
-        ops.fnArray[i] === OPS.paintImageXObject ||
-        ops.fnArray[i] === OPS.paintInlineImageXObject) {
-        const name = ops.argsArray[i][0]
-        const img = await page.objs.get(name)
-        const { width, height, kind } = img
-        const bytes = img.data.length
-        const channels = bytes / width / height
-        const file = Path.join(dst, `${name}.png`)
-        await sharp(img.data, {
-          raw: { width, height, channels }
-        }).toFile(file)
-        const event = { name, kind, width, height, channels, bytes, file }
-        emit('image', event)
-        images.push(event)
+      try {
+        if (
+          ops.fnArray[i] === OPS.paintJpegXObject ||
+          ops.fnArray[i] === OPS.paintImageXObject ||
+          ops.fnArray[i] === OPS.paintInlineImageXObject) {
+          const name = ops.argsArray[i][0]
+          const img = await page.objs.get(name)
+          const { width, height, kind } = img
+          const bytes = img.data.length
+          const channels = bytes / width / height
+          const file = Path.join(dst, `${name}.png`)
+          await sharp(img.data, {
+            raw: { width, height, channels }
+          }).toFile(file)
+          const event = { name, kind, width, height, channels, bytes, file }
+          emit('image', event)
+          images.push(event)
+        }
+      } catch (error) {
+        emit('error', error)
       }
     }
   }
