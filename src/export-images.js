08421d51ba6a5e5fe624d36fa63aa765098dc904
Marc Ablay <marc.ablay@gmail.com>
2023-08-18 07:18:18 +0200

    bugfix improved image extraction

--- src/export-images.js
+++ src/export-images.js
@@ -17,7 +17,7 @@ export function exportImagesEvents (src, dst) {
   return promise
 }
 
-export async function exportImages (src, dst, emit = () => {}) {
+export async function exportImages (src, dst, emit = (name, data) => {}) {
   const doc = await getDocument(src).promise
   const pageCount = doc._pdfInfo.numPages
   emit('load', { pageCount })
@@ -29,14 +29,21 @@ export async function exportImages (src, dst, emit = () => {}) {
     for (let i = 0; i < ops.fnArray.length; i++) {
       try {
         if (
-          ops.fnArray[i] === OPS.paintJpegXObject ||
           ops.fnArray[i] === OPS.paintImageXObject ||
-          ops.fnArray[i] === OPS.paintInlineImageXObject) {
+          ops.fnArray[i] === OPS.paintInlineImageXObject
+        ) {
           const name = ops.argsArray[i][0]
-          const img = await page.objs.get(name)
+          const common = await page.commonObjs.has(name)
+          const img = await (common
+            ? page.commonObjs.get(name)
+            : page.objs.get(name)
+          )
           const { width, height, kind } = img
           const bytes = img.data.length
           const channels = bytes / width / height
+          if (!(channels === 1 || channels === 2 || channels === 3 || channels === 4)) {
+            throw new Error(`Invalid image channel: ${channels} for image ${name} on page ${page}`)
+          }
           const file = Path.join(dst, `${name}.png`)
           await sharp(img.data, {
             raw: { width, height, channels }
