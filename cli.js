2486bf0eca795c3a7e429af568ce3ec2a71c7da4
Marc Ablay <marc.ablay@gmail.com>
2023-08-18 07:16:29 +0200

    continue extraction on error

--- cli.js
+++ cli.js
@@ -23,6 +23,7 @@ if (args.includes('-h') || args.includes('--help')) {
   exportImagesEvents(file, dir)
     .on('load', event => console.log('Exporting images from', event.pageCount, 'pages...'))
     .on('image', ({ file, width, height }) => console.log(file, `(${width}x${height})`))
+    .on('error', console.error)
     .on('done', images => console.log(images.length, 'images exported'))
     .catch(console.error)
 }
